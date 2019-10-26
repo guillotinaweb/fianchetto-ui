7a7e253c09bd0f98526166f20af69de08c7ee6f3
Jon Pentland <jon.pentland@pretagov.co.uk>
2019-10-26 15:54:08 +0200

    add grange

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+import { GrangeRootModule } from 'grange';
+import { TraversalModule } from 'angular-traversal';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TraversalModule,
+    GrangeRootModule.forRoot(),
+    StoreModule.forRoot({}),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'CONFIGURATION',
+      useValue: {
+        BACKEND_URL: environment.backend,
+        CLIENT_TIMEOUT: 5000,
+      },
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
